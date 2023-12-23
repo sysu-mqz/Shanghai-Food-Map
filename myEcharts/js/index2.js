a7ded39618c8393c60f4e59fe1d7e85b41a63d50
sysu_hgy212 <huanggy28@mail2.sysu.edu.cn>
2023-12-23 15:28:51 +0800

    代码优化

--- myEcharts/js/index2.js
+++ myEcharts/js/index2.js
@@ -18,7 +18,7 @@
     return shuffled.slice(0, size);
   }
 
-  // 统计各个类别的名称和数量 
+  // 统计各个类别的名称和数量
   function countCategories (data) {  
     let categories = new Set();  
     let categoryCounts = {};
@@ -92,97 +92,21 @@
       }
     ]
   };
-  // var option = {
-  //   color: ["#2f89cf"],
-  //   tooltip: {
-  //     trigger: "axis",
-  //     axisPointer: {
-  //       // 坐标轴指示器，坐标轴触发有效
-  //       type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
-  //     }
-  //   },
-  //   // 修改图表的大小
-  //   grid: {
-  //     left: "0%",
-  //     top: "10px",
-  //     right: "0%",
-  //     bottom: "4%",
-  //     containLabel: true
-  //   },
-  //   xAxis: [
-  //     {
-  //       type: "category",
-  //       data: [
-  //         "旅游行业",
-  //         "教育培训",
-  //         "游戏行业",
-  //         "医疗行业",
-  //         "电商行业",
-  //         "社交行业",
-  //         "金融行业"
-  //       ],
-  //       axisTick: {
-  //         alignWithLabel: true
-  //       },
-  //       // 修改刻度标签 相关样式
-  //       axisLabel: {
-  //         color: "rgba(255,255,255,.6) ",
-  //         fontSize: "12"
-  //       },
-  //       // 不显示x坐标轴的样式
-  //       axisLine: {
-  //         show: false
-  //       }
-  //     }
-  //   ],
-  //   yAxis: [
-  //     {
-  //       type: "value",
-  //       // 修改刻度标签 相关样式
-  //       axisLabel: {
-  //         color: "rgba(255,255,255,.6) ",
-  //         fontSize: 12
-  //       },
-  //       // y轴的线条改为了 2像素
-  //       axisLine: {
-  //         lineStyle: {
-  //           color: "rgba(255,255,255,.1)",
-  //           width: 2
-  //         }
-  //       },
-  //       // y轴分割线的颜色
-  //       splitLine: {
-  //         lineStyle: {
-  //           color: "rgba(255,255,255,.1)"
-  //         }
-  //       }
-  //     }
-  //   ],
-  //   series: [
-  //     {
-  //       name: "直接访问",
-  //       type: "bar",
-  //       barWidth: "35%",
-  //       data: [200, 300, 300, 900, 1500, 1200, 600],
-  //       itemStyle: {
-  //         // 修改柱子圆角
-  //         barBorderRadius: 5
-  //       }
-  //     }
-  //   ]
-  // };
+  
   // 3. 把配置项给实例对象
   myChart.setOption(option);
+
   // 4. 让图表跟随屏幕自动的去适应
   window.addEventListener("resize", function() {
     myChart.resize();
   });
 })();
 
-// 柱状图2
+// 尝试利用平行坐标系展示美食的点评数、口味、环境、服务和人均消费
 (function() {
   // 1. 实例化对象
   var myChart = echarts.init(document.querySelector(".bar2 .chart"));
+
   // 2. 指定配置和数据
   // 随机获取一个指定长度的数组
   function getRandomSubarray(arr, size) {
@@ -198,7 +122,6 @@
 
   // 生成不同美食类别的parallel数据结构，希望实现类似的效果：https://echarts.apache.org/examples/zh/editor.html?c=parallel-aqi
   function generateData(data) {
-    console.log(data);
     let finalData = [];
     let categories = new Set();
     let selectedCategories = [];
@@ -220,7 +143,6 @@
 
     categories.forEach((item) => {
       if (categoryCounts[item] / data.length >= 0.1) {
-        console.log('我进来了 category: ' + item);
         let newData = data.filter(item2 => item2.category === item); 
         let obj = {};
         obj.name = item;
@@ -269,9 +191,8 @@
           averageExpanse: item.人均消费,
       };
   });
+  // 生成占比不小于10%的美食类别对应的数据集
   var result = generateData(selectedData);
-  console.log(result.finalData);
-  console.log(result.selectedCategories);
 
   var option = {
     parallelAxis: [
@@ -299,112 +220,11 @@
     //   data: generateParallelData(selectedData),
     // },
     series: result.finalData,
-    // visualMap: {
-    //   show: true,
-    //   min: 0,
-    //   max: 150,
-    //   dimension: 4,
-    //   inRange: {
-    //     color: ['#d94e5d', '#eac736', '#50a3ba'].reverse()
-    //     // colorAlpha: [0, 1]
-    //   }
-    // },
   };
-
-  // var option = {
-  //   grid: {
-  //     top: "10%",
-  //     left: "22%",
-  //     bottom: "10%"
-  //     // containLabel: true
-  //   },
-  //   // 不显示x轴的相关信息
-  //   xAxis: {
-  //     show: false
-  //   },
-  //   yAxis: [
-  //     {
-  //       type: "category",
-  //       inverse: true,
-  //       data: ["HTML5", "CSS3", "javascript", "VUE", "NODE"],
-  //       // 不显示y轴的线
-  //       axisLine: {
-  //         show: false
-  //       },
-  //       // 不显示刻度
-  //       axisTick: {
-  //         show: false
-  //       },
-  //       // 把刻度标签里面的文字颜色设置为白色
-  //       axisLabel: {
-  //         color: "#fff"
-  //       }
-  //     },
-  //     {
-  //       data: [702, 350, 610, 793, 664],
-  //       inverse: true,
-  //       // 不显示y轴的线
-  //       axisLine: {
-  //         show: false
-  //       },
-  //       // 不显示刻度
-  //       axisTick: {
-  //         show: false
-  //       },
-  //       // 把刻度标签里面的文字颜色设置为白色
-  //       axisLabel: {
-  //         color: "#fff"
-  //       }
-  //     }
-  //   ],
-  //   series: [
-  //     {
-  //       name: "条",
-  //       type: "bar",
-  //       data: [70, 34, 60, 78, 69],
-  //       yAxisIndex: 0,
-  //       // 修改第一组柱子的圆角
-  //       itemStyle: {
-  //         barBorderRadius: 20,
-  //         // 此时的color 可以修改柱子的颜色
-  //         color: function(params) {
-  //           // params 传进来的是柱子对象
-  //           // console.log(params);
-  //           // dataIndex 是当前柱子的索引号
-  //           return myColor[params.dataIndex];
-  //         }
-  //       },
-  //       // 柱子之间的距离
-  //       barCategoryGap: 50,
-  //       //柱子的宽度
-  //       barWidth: 10,
-  //       // 显示柱子内的文字
-  //       label: {
-  //         show: true,
-  //         position: "inside",
-  //         // {c} 会自动的解析为 数据  data里面的数据
-  //         formatter: "{c}%"
-  //       }
-  //     },
-  //     {
-  //       name: "框",
-  //       type: "bar",
-  //       barCategoryGap: 50,
-  //       barWidth: 15,
-  //       yAxisIndex: 1,
-  //       data: [100, 100, 100, 100, 100],
-  //       itemStyle: {
-  //         color: "none",
-  //         borderColor: "#00c1de",
-  //         borderWidth: 3,
-  //         barBorderRadius: 15
-  //       }
-  //     }
-  //   ]
-  // };
 
   // 3. 把配置给实例对象
   myChart.setOption(option);
+
   // 4. 让图表跟随屏幕自动的去适应
   window.addEventListener("resize", function() {
     myChart.resize();
@@ -412,7 +232,7 @@
 })();
 
 // 如果改成四个图，需要隐藏以下代码，否则之后的图像无法显示
-// 折线图1模块制作
+// // 折线图1模块制作
 // (function() {
 //   var yearData = [
 //     {
@@ -542,7 +362,7 @@
 //   });
 // })();
 
-// 折线图2 模块制作
+// // 折线图2 模块制作
 // (function() {
 //   var myChart = echarts.init(document.querySelector(".line2 .chart"));
 //   var option = {
@@ -927,6 +747,7 @@
   });
 })();
 
+// 核心部分，实现美食地图的展示
 (function () {
     myChart = echarts.init(document.querySelector(".map .chart"));
     function getRandomSubarray(arr, size) {
